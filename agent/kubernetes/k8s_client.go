beeb3fd717119a6f657acc1dd500c738c8c984af
milo <miloyuans@gmail.com>
2025-10-25 22:22:38 +0400

    up good code

--- agent/kubernetes/k8s_client.go
+++ agent/kubernetes/k8s_client.go
@@ -10,7 +10,7 @@ import (
 	appsv1 "k8s.io/api/apps/v1"
 	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	"k8s.io/apimachinery/pkg/labels"
+	//"k8s.io/apimachinery/pkg/labels"
 	"k8s.io/apimachinery/pkg/util/intstr"
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/rest"
